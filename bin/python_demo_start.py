3f11048867381905e5b4b498aec4ab1a4be5ef5c
PIG-HAVE-DRAGON <zhuyoulong@csii.com.cn>
2018-12-19 20:36:19 +0800

    Update python_demo_start.py
    For my youth!

--- bin/python_demo_start.py
+++ bin/python_demo_start.py
@@ -2,3 +2,9 @@ import re
 print("Hello world github! you are good!")
 print("I like python! I don't want to die!")
 print("God bless me!")
+
+reg_for_id_no = "[0-9]{17}[xX0-9]"
+
+mo = re.match(reg_for_id_no, '411524199507037213')
+
+print(mo.group(0))
