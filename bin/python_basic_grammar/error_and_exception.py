35f3bcc4cb2d887d3b86cf07fb4edff7da455dd3
PIG-HAVE-DRAGON <zhuyoulong@csii.com.cn>
2018-12-20 18:49:14 +0800

    Update error_and_exception.py
    error exception

--- bin/python_basic_grammar/error_and_exception.py
+++ bin/python_basic_grammar/error_and_exception.py
@@ -17,3 +17,23 @@ except ValueError:
     print("please enter a number")
 except:
     print("System unknow error!")
+    raise
+
+
+# 自定义异常
+class MyError(Exception):
+
+    def __init__(self, msg):
+        self.msg = msg
+    
+    def __str__(self):
+        return repr(self.msg)
+
+try:
+    raise MyError("This is my Error")
+except MyError as e:
+    print(e.msg)
+finally:
+    print("Goodbye!")
+
+
