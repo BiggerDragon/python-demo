f385f47b30412a14a947929465a82fa85d688aa6
PIG-HAVE-DRAGON <zhuyoulong@csii.com.cn>
2018-12-20 12:51:48 +0800

    Update demo01.py
    For understanding the concept of class!

--- bin/python_basic_grammar/demo01.py
+++ bin/python_basic_grammar/demo01.py
@@ -5,13 +5,21 @@ class Activity:
     __decription = "This is private attr for Activity class inner use!"
     _location = 'Asia'
     country = 'China'
+    activity_attrs_keys = ['activity_no', 'activity_name', 'activity_status', 'activity_begin_date',
+    'activity_end_date', 'create_date','activity_des']
 
     def __init__(self, **kwargs):
         print(self.__decription)
-        self.activity_attrs = kwargs
+        self.activity_attrs = {}
+        for key in self.activity_attrs_keys:
+            self.activity_attrs[key] = kwargs[key]
+        
     
     @staticmethod
     def checkActivity(self):
+        """
+        static method is simple method!
+        """
         if self is None:
             raise Exception('no activity instance!')
         if self.activity_attrs['activity_begin_date'] is None or self.activity_attrs['activity_end_date'] is None:
@@ -22,12 +30,32 @@ class Activity:
             raise Exception('activity_begin_date >= activity_end_date')
     
     def get_act_time(self):
+        """
+        instance method must bind an instance
+        """
         return {'activity_begin_date':self.activity_attrs['activity_begin_date'],
                 'activity_end_date':self.activity_attrs['activity_end_date']}
+    
+    @classmethod
+    def print_class_vars(cls):
+        """
+        classmethod must bind class object(cls)
+        """
+        print("activity_attrs_keys=", cls.activity_attrs_keys)
+        print("__decription=", cls.__decription)
+        print("location=", cls._location)
+        print("country=", cls.country)
+        pass
         
         
 
-act2 = Activity(activity_name='注册送积分', activity_begin_date='2018-11-02', activity_end_date='2018-12-30')
+act2 = Activity(activity_no=time.time(), 
+                activity_name='注册送积分', 
+                activity_begin_date='2018-11-02', 
+                activity_end_date='2018-12-30',
+                activity_status="P",
+                activity_des="This is a activity!",
+                create_date=time.time())
 print(act2.activity_attrs)
 print(type(act2.activity_attrs))
 
@@ -37,7 +65,13 @@ print(act2.activity_attrs)
 print(act2.__getattribute__('activity_attrs'))
 
 print("创建活动3!")
-act_attrs = {'activity_name':'购买VIP会员参与抽奖','activity_begin_date':'2018-12-11', 'activity_end_date':'2018-12-30'}
+act_attrs = {'activity_name':'购买VIP会员参与抽奖',
+                'activity_begin_date':'2018-12-11',
+                'activity_end_date':'2018-12-30',
+                'activity_no':time.time(),
+                'activity_status':'P',
+                'activity_des':'This is a draw activity!',
+                'create_date':time.time()}
 act3 = Activity(**act_attrs)
 
 Activity.checkActivity(act3)
@@ -46,4 +80,6 @@ print(act3.get_act_time())
 
 print(act3.country)
 print(act3._location)
+
+Activity.print_class_vars()
 
